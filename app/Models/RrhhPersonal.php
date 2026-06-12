0b2048dedadd89139fda4813c372a76e15a19376
luigui29 <luiguiunity29@pm.me>
2026-06-12 18:27:39 -0400

    feat: configuracion de conexiones a db sap

---
+++ app/Models/RrhhPersonal.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RrhhPersonal extends Model
+{
+    protected $connection = 'pgsql_sap';
+    protected $table = 'tbl_rrhh_personal';
+    protected $primaryKey = 'ficha';
+    protected $keyType = 'int';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ficha',
+        'nombre_empleado',
+        'texto_cargo',
+        'texto_unidad',
+        'texto_gerencia',
+        'supervisor',
+        'texto_status',
+        'centro_costo',
+        'centro_txt'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id_usuario', 'ficha');
+    }
+}
--- config/database.php
+++ config/database.php
@@ -99,6 +99,21 @@ return [
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
+        'pgsql_sap' => [
+            'driver' => 'pgsql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE_SAP', 'db_sap'),
+            'username' => env('DB_USERNAME', 'postgresql'),
+            'password' => env('DB_PASSWORD', 'Wuasa.92'),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
